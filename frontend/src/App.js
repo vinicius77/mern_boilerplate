8ed9c2f389aac13a48b85a14031bdd8b98b749ad
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-02-19 16:43:57 +0200

    feat(upgrading-deprecated): upgrades deprecated dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,73 +1,72 @@
-import React, { useState, useEffect } from 'react'; // React Hooks
-
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import NewProduct from './components/Products/NewProduct';
 import ProductList from './components/Products/ProductList';
 import './App.css';
 
 function App() {
-  const [loadedProducts, setLoadedProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+	const [loadedProducts, setLoadedProducts] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:5000/products');
+	useEffect(() => {
+		const fetchProducts = async () => {
+			setIsLoading(true);
+			const response = await fetch('http://localhost:5000/products');
 
-      const responseData = await response.json();
+			const responseData = await response.json();
 
-      setLoadedProducts(responseData.products);
-      setIsLoading(false);
-    };
-    fetchProducts();
-  }, []);
+			setLoadedProducts(responseData.products);
+			setIsLoading(false);
+		};
+		fetchProducts();
+	}, []);
 
-  const addProductHandler = async (productName, productPrice) => {
-    try {
-      const newProduct = {
-        title: productName,
-        price: +productPrice
-      };
-      let hasError = false;
-      const response = await fetch('http://localhost:5000/product', {
-        method: 'POST',
-        body: JSON.stringify(newProduct),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+	const addProductHandler = async (productName, productPrice) => {
+		try {
+			const newProduct = {
+				title: productName,
+				price: +productPrice,
+			};
+			let hasError = false;
+			const response = await fetch('http://localhost:5000/product', {
+				method: 'POST',
+				body: JSON.stringify(newProduct),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
 
-      if (!response.ok) {
-        hasError = true;
-      }
+			if (!response.ok) {
+				hasError = true;
+			}
 
-      const responseData = await response.json();
+			const responseData = await response.json();
 
-      if (hasError) {
-        throw new Error(responseData.message);
-      }
+			if (hasError) {
+				throw new Error(responseData.message);
+			}
 
-      setLoadedProducts(prevProducts => {
-        return prevProducts.concat({
-          ...newProduct,
-          id: responseData.product.id
-        });
-      });
-    } catch (error) {
-      alert(error.message || 'Something Went Wrong!!');
-    }
-  };
+			setLoadedProducts((prevProducts) => {
+				return prevProducts.concat({
+					...newProduct,
+					id: responseData.product.id,
+				});
+			});
+		} catch (error) {
+			alert(error.message || 'Something Went Wrong!!');
+		}
+	};
 
-  return (
-    <React.Fragment>
-      <Header />
-      <main>
-        <NewProduct onAddProduct={addProductHandler} />
-        {isLoading && <p className='loader'>Loading...</p>}
-        {!isLoading && <ProductList items={loadedProducts} />}
-      </main>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Header />
+			<main>
+				<NewProduct onAddProduct={addProductHandler} />
+				{isLoading && <p className="loader">Loading...</p>}
+				{!isLoading && <ProductList items={loadedProducts} />}
+			</main>
+		</React.Fragment>
+	);
 }
 
 export default App;
